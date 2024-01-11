9338044bca27af624b1cf69a9a9df7b5308e2e05
ebaybasics <kccleancarpets@gmail.com>
2024-01-11 14:27:33 -0600

    Add game logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'strict'
 let PLAYER_CHOICE = -1;
-let QUIT = 0;
+let EXIT = 0;
 
 
 
@@ -10,14 +10,52 @@ const compChoice = () => {
 };
 
 
-const gameRun = () => {
-    const pNumChoice = prompt('Rock Paper or Scissors (1,2,3)');
-    const cNumChoice = compChoice();
+// const gameRun = () => {
+//     const pNumChoice = prompt('Rock Paper or Scissors (1,2,3)');
+//     const cNumChoice = compChoice();
 
-    const gameDict = {
-        1: 'Rock',
-        2: 'Paper',
-        3: 'Scissors',
+//     const gameDict = {
+//         1: 'Rock',
+//         2: 'Paper',
+//         3: 'Scissors',
+//         11: 'Tied',
+//         12: 'Lost',
+//         13: 'Won',
+//         21: 'Won',
+//         22: 'Tied',
+//         23: 'Lost',
+//         31: 'Lost',
+//         32: 'Won',
+//         33: 'Tied'
+//     };
+
+//     const playerChoice = gameDict[pNumChoice];
+//     const cChoice = gameDict[cNumChoice];
+//     const result = gameDict[`${pNumChoice}${cNumChoice}`];
+
+//     switch (playerChoice) {
+//         case gameDict[1]:
+//             console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
+            
+//             break;
+//         case gameDict[2]:
+//             console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
+//             break;
+//         case gameDict[3]:
+//             console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
+//             break;   
+//     }
+//     quit.addEventListener('click', e => {
+//         e.preventDefault();
+//         QUIT = 1;
+//     })
+// }
+
+// gameRun();
+
+// ---------- DOM Manipulation -------- //
+
+    const resDict = {
         11: 'Tied',
         12: 'Lost',
         13: 'Won',
@@ -28,32 +66,6 @@ const gameRun = () => {
         32: 'Won',
         33: 'Tied'
     };
-
-    const playerChoice = gameDict[pNumChoice];
-    const cChoice = gameDict[cNumChoice];
-    const result = gameDict[`${pNumChoice}${cNumChoice}`];
-
-    switch (playerChoice) {
-        case gameDict[1]:
-            console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
-            
-            break;
-        case gameDict[2]:
-            console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
-            break;
-        case gameDict[3]:
-            console.log(`You chose ${playerChoice}, He chose ${cChoice} and you ${result}`);
-            break;   
-    }
-    quit.addEventListener('click', e => {
-        e.preventDefault();
-        QUIT = 1;
-    })
-}
-
-// gameRun();
-
-// ---------- DOM Manipulation -------- //
 
 const rButton = document.querySelector('.rock-button');
 const pButton = document.querySelector('.paper-button');
@@ -67,7 +79,13 @@ const compRock = document.querySelector('.comp-rock');
 const compPaper = document.querySelector('.comp-paper');
 const compScissors = document.querySelector('.comp-scissors');
 
-const quit = document.querySelector('.quit');
+const hScore = document.querySelector('.human-score');
+const cScore = document.querySelector('.comp-score');
+const reset = document.querySelector('.reset-button');
+
+// --------------init-------------//
+hScore.textContent = 0;
+cScore.textContent = 0;
 
 
 rButton.addEventListener('click', (e) => {
@@ -78,22 +96,28 @@ rButton.addEventListener('click', (e) => {
     humanPaper.classList.add('hide');
 
     const res = compChoice();
-    console.log(res);
+    const statusKey = `${PLAYER_CHOICE}${res}`
+    console.log(statusKey);
+    const roundResult = resDict[statusKey];
     if (res === 1 ) {
         compRock.classList.remove('hide');
         compScissors.classList.add('hide');
         compPaper.classList.add('hide');
     } else if (res === 2) {
         compRock.classList.add('hide');
-        compScissors.classList.remove('hide');
-        compPaper.classList.add('hide');
-    } else if (res === 3) {
-        compRock.classList.add('hide');
         compScissors.classList.add('hide');
         compPaper.classList.remove('hide');
+    } else if (res === 3) {
+        compRock.classList.add('hide');
+        compScissors.classList.remove('hide');
+        compPaper.classList.add('hide');
     }
 
-
+    if (roundResult === 'Won') {
+        hScore.textContent = Number(hScore.textContent) + 1; 
+    } else if (roundResult === 'Lost') {
+        cScore.textContent = Number(cScore.textContent) + 1; 
+    }
     
 })
 
@@ -101,49 +125,76 @@ pButton.addEventListener('click', (e) => {
     e.preventDefault();
     PLAYER_CHOICE = 2;
     humanRock.classList.add('hide');
-    humanScissors.classList.remove('hide');
-    humanPaper.classList.add('hide');
+    humanScissors.classList.add('hide');
+    humanPaper.classList.remove('hide');
 
     const res = compChoice();
-    console.log(res);
+    const statusKey = `${PLAYER_CHOICE}${res}`
+    console.log(statusKey);
+    const roundResult = resDict[statusKey];
     if (res === 1 ) {
         compRock.classList.remove('hide');
         compScissors.classList.add('hide');
         compPaper.classList.add('hide');
     } else if (res === 2) {
         compRock.classList.add('hide');
-        compScissors.classList.remove('hide');
-        compPaper.classList.add('hide');
-    } else if (res === 3) {
-        compRock.classList.add('hide');
         compScissors.classList.add('hide');
         compPaper.classList.remove('hide');
+    } else if (res === 3) {
+        compRock.classList.add('hide');
+        compScissors.classList.remove('hide');
+        compPaper.classList.add('hide');
     }
+
+    if (roundResult === 'Won') {
+        hScore.textContent = Number(hScore.textContent) + 1; 
+    } else if (roundResult === 'Lost') {
+        cScore.textContent = Number(cScore.textContent) + 1; 
+    }
+
 })
 
 sButton.addEventListener('click', (e) => {
     e.preventDefault();
     PLAYER_CHOICE = 3;
     humanRock.classList.add('hide');
-    humanScissors.classList.add('hide');
-    humanPaper.classList.remove('hide');
+    humanScissors.classList.remove('hide');
+    humanPaper.classList.add('hide');
 
     const res = compChoice();
-    console.log(res);
+    const statusKey = `${PLAYER_CHOICE}${res}`
+    console.log(statusKey);
+    const roundResult = resDict[statusKey];
     if (res === 1 ) {
         compRock.classList.remove('hide');
         compScissors.classList.add('hide');
         compPaper.classList.add('hide');
     } else if (res === 2) {
         compRock.classList.add('hide');
-        compScissors.classList.remove('hide');
-        compPaper.classList.add('hide');
-    } else if (res === 3) {
-        compRock.classList.add('hide');
         compScissors.classList.add('hide');
         compPaper.classList.remove('hide');
+    } else if (res === 3) {
+        compRock.classList.add('hide');
+        compScissors.classList.remove('hide');
+        compPaper.classList.add('hide');
+    }
+
+    if (roundResult === 'Won') {
+        hScore.textContent = Number(hScore.textContent) + 1; 
+    } else if (roundResult === 'Lost') {
+        cScore.textContent = Number(cScore.textContent) + 1; 
     }
 })
 
-
+reset.addEventListener('click', (e) => {
+    e.preventDefault();
+    hScore.textContent = 0;
+    cScore.textContent = 0;
+    humanRock.classList.add('hide');
+    humanScissors.classList.add('hide');
+    humanPaper.classList.add('hide');
+    compRock.classList.add('hide');
+    compScissors.classList.add('hide');
+    compPaper.classList.add('hide');
+})
 
